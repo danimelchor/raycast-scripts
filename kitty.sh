b3ada7f423ab59735950deb63a54b3817797ba4c
danimelchor <dmh672@gmail.com>
2023-02-22 22:36:13 -0500

    first commit

---
+++ kitty.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Kitty
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName Kitty
+
+# Documentation:
+# @raycast.description Opens kitty
+# @raycast.author Daniel Melchor
+# @raycast.authorURL https://danielmelchor.com/
+
+cd ~;
+kitty;
---
+++ neovim.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Neovim
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName Neovim
+
+# Documentation:
+# @raycast.description Neovim
+# @raycast.author Daniel Melchor
+# @raycast.authorURL https://danielmelchor.com/
+
+cd ~;
+kitty --start-as=maximized -o hide_window_decorations=yes nvim;
+exit 0;
